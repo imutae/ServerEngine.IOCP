9ceb229898a8fc2404a97e8211e5e1d2be101ba3
imutae <imutae.lab@gmail.com>
2026-04-28 20:47:52 +0900

    include 수정

--- pch.h
+++ pch.h
@@ -1,7 +1,12 @@
-﻿#pragma once
+#pragma once
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 
 #include <winsock2.h>
 #include <mswsock.h>
